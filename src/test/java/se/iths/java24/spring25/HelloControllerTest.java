433ffce0e611e17f42963ca35a5eb8d6f7ecb72e
Martin Blomberg <martin.blomberg@outlook.com>
2025-03-20 12:07:21 +0100

    Organize imports

--- src/test/java/se/iths/java24/spring25/HelloControllerTest.java
+++ src/test/java/se/iths/java24/spring25/HelloControllerTest.java
@@ -1,10 +1,8 @@
 package se.iths.java24.spring25;
 
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import se.iths.java24.spring25.domain.PlaygroundService;
@@ -12,7 +10,9 @@ import se.iths.java24.spring25.domain.entity.Playground;
 
 import java.util.List;
 
+import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -34,10 +34,10 @@ class HelloControllerTest {
 
         mockMvc.perform(get("/playgrounds"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentType(APPLICATION_JSON))
                 .andExpect(jsonPath("$[0].name").value("test"))
                 .andExpect(jsonPath("$[0].id").value(1))
                 .andExpect(jsonPath("$").isArray())
-                .andExpect(jsonPath("$.length()", Matchers.is(1)));
+                .andExpect(jsonPath("$.length()", is(1)));
     }
 }
